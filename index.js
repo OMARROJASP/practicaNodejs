497f053d91d979f70318abf7abe8efdcd4ef7551
maximo261199 <u201920341@upc.edu.pe>
2023-02-20 18:13:56 -0500

    backend-deploy

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ import express from 'express';
 import authRouter from './routers/auth.route.js';
 import cookieParser from 'cookie-parser';
 import linkRouter from "./routers/link.route.js"
+import redirectRoute from "./routers/redirect.route.js";
+import cors from 'cors'
+const whileList = [process.env.ORIGIN1, process.env.ORIGIN2  ]
 
 const app = express();
+
+app.use(cors({
+    origin: function (origin, callback){
+    if(whileList.includes(origin)){
+        return callback(null, origin);
+    }
+    return callback(
+        "Error de CORS origin: " + origin + " No autorizado!"
+    )
+    }
+}))
 app.use(express.json());
 app.use(cookieParser())
+app.use('/', redirectRoute)
 app.use('/api/v1/auth', authRouter)
 app.use("/api/v1/links", linkRouter)
 //solo para ejemplo
-app.use(express.static('public'))
+//app.use(express.static('public'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,() => console.log("hay caray 😂😂😂😂por fin http://localhost:" +PORT))
--- routers/link.route.js
+++ routers/link.route.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { createLink, getLink, getLinks, removeLink } from "../controllers/link.controller.js";
+import {createLink, getLink, getLinks, removeLink, updateLink} from "../controllers/link.controller.js";
 import { requiereToken } from "../middlewares/requiereToken.js";
 import { bodyLinkValidator, paramLinkValidator } from "../middlewares/validationManeger.js";
 const router = Router()
@@ -11,9 +11,9 @@ const router = Router()
 // DELETE           /api/v1/links/:id       remove link
 
 router.get('/',requiereToken,getLinks )
-router.get("/:id",requiereToken,  getLink)
+router.get("/:nanoLink",  getLink)
 router.post('/',requiereToken,bodyLinkValidator,createLink)
 router.delete("/:id", requiereToken,paramLinkValidator, removeLink)
-
+router.patch("/:id", requiereToken,paramLinkValidator,bodyLinkValidator, updateLink)
 
 export default router;
